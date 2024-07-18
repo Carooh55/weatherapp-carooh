272e491c9037686fbbc5b70133b8811a36e41289
Carooh55 <carolinemakhadzi40@gmail.com>
2024-07-19 01:17:36 +0200

    changed the apikey

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+    const apiKey = "eac360db5fc86ft86450f3693e73o43f";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
